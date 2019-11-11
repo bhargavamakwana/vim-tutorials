8619dfe6c610b128b961a504bcf22140ffadbb25
Bhargav Makwana <bhargavamakwana@gmail.com>
2019-11-11 15:21:30 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,14 @@
 
 # Vim Setup Script
 
+# get username
+user=$(whoami)
+
+#Install Vim from repository
 sudo apt-get install vim
+
+#Copy and setup vim plugins.
 cp vimrc ~/.vimrc
 
+# Link settings for root user also.
+ln -s /home/$user/.vimrc /root/.vimrc
